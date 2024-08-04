7a48a0663eb93a9d5e7bb7474ba8ab1ee7c8cc34
badza <dusan.badza@yahoo.com>
2024-08-04 16:17:29 +0200

    coca cola model

---
+++ components/model-viewer.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import React, { Suspense, useRef, useState, useEffect } from "react";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { useSpring, animated, config } from "@react-spring/three";
+import { Mesh, Box3, Vector3 } from "three";
+
+// Define the props types
+interface ModelProps {
+  url: string;
+}
+
+const Model: React.FC<ModelProps> = ({ url }) => {
+  const gltf = useLoader(GLTFLoader, url);
+  const modelRef = useRef<Mesh>();
+  const [active, setActive] = useState(false);
+
+  // Center and scale the model
+  useEffect(() => {
+    if (modelRef.current) {
+      const box = new Box3().setFromObject(modelRef.current);
+      const center = new Vector3();
+      box.getCenter(center);
+      modelRef.current.position.sub(center);
+
+      // Scale the model
+      // modelRef.current.scale.set(20, 20, 20);
+    }
+  }, [gltf]);
+
+  // Animation for sliding in from the right
+  const { x } = useSpring({
+    from: { x: 10 },
+    x: active ? 0 : 10,
+    config: config.molasses,
+  });
+
+  useEffect(() => {
+    const timer = setTimeout(() => setActive(true), 100);
+    return () => clearTimeout(timer);
+  }, []);
+
+  useFrame(() => {
+    if (modelRef.current) {
+      modelRef.current.rotation.y += 0.01;
+    }
+  });
+
+  return (
+    <animated.group position-x={x}>
+      <primitive ref={modelRef} object={gltf.scene} />
+    </animated.group>
+  );
+};
+
+// Define the props types
+interface ModelViewerProps {
+  url: string;
+}
+
+const ModelViewer: React.FC<ModelViewerProps> = ({ url }) => {
+  return (
+    <div className="h-screen w-full flex items-center justify-center">
+      <Canvas camera={{ position: [0, 0, 0.5], fov: 25 }} gl={{ alpha: true }}>
+        <Suspense fallback={null}>
+          <ambientLight intensity={1} />
+          <directionalLight position={[2, 2, 2]} intensity={30} />
+          <Model url={url} />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
+};
+
+export default ModelViewer;
